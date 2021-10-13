3aed3edf0121bfe2b3462b9bc7f3e10be78f0d9d
André Lopes <andrelupus1@hotmail.com>
2021-10-13 18:28:08 -0300

    CRUD - Diarista

---
+++ app/Models/Diarista.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Diarista extends Model
+{
+    use HasFactory;
+    /**
+     * Defini os campos que podem ser gravados
+     *
+     * @var array
+     */
+    protected $fillable = ['nome_completo', 'cpf', 'email', 'telefone', 'logradouro', 'numero', 'complemento', 'bairro', 'cidade', 'estado', 'cep', 'codigo_ibge', 'foto_usuario'];
+}
