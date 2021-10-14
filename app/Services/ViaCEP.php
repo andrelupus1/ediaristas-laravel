5d6a061608b9d8797db1ad4d6db8674d4c3da86e
André Lopes <andrelupus1@hotmail.com>
2021-10-13 23:30:20 -0300

    Busca CEP

---
+++ app/Services/ViaCEP.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\Http;
+
+class ViaCEP
+{
+
+    public function Buscar(string $cep)
+    {
+        $url = sprintf('https://viacep.com.br/ws/%s/json/', $cep);
+
+        $resposta = Http::get($url);
+
+        if ($resposta->failed()) {
+            return false;
+        }
+
+        $dados = $resposta->json();
+
+        if (isset($dados['erro']) && $dados['erro'] === true) {
+            return false;
+        }
+
+        return $dados;
+    }
+}
